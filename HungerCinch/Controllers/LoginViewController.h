d580dcca8c1c520766444152271deddfc56ed764
eshuang <eshuang@ucdavis.edu>
2013-11-14 17:17:39 -0800

    Added login

---
+++ HungerCinch/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  HungerCinch
+//
+//  Created by Erhuang2 on 11/14/13.
+//  Copyright (c) 2013 Shane Hartman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UITableViewController
+
+@end
